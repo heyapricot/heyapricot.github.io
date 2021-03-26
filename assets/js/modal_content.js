cf76c84324b2cb947be18201924b1debb036d7b2
Alvaro Sanchez <heyapricot@fastmail.com>
2021-03-25 20:11:15 -0600

    Added screenshots for the project cards and modals

--- assets/js/modal_content.js
+++ assets/js/modal_content.js
@@ -1,11 +1,22 @@
 const showcaseModalData = {
-  "dataDashboardHealthcare": {
-    "title": "Data Dashboard Healthcare",
-    "path": 'assets/images/project1.jpg'
+  "battleshipJS": {
+    "title": "Battleship.js",
+    "path": "assets/images/battleship.gif",
+    "live": "https://heyapricot.github.io/battleship.js",
+    "repo": "https://github.com/heyapricot/battleship.js"
   },
-  "multiPostStories": {
-    "title": "Multi-Post Stories",
-    "path": 'assets/images/project2.jpg'
+  "ticTacToeJS": {
+    "title": "TicTacToe.js",
+    "path": "assets/images/tic_tac_toe.gif"
+  },
+  "toDoJS": {
+    "title": "ToDo.js",
+    "path": "assets/images/to_do.gif"
+  },
+  "weatherJS": {
+    "title": "Weateher.js",
+    "path": "assets/images/weather.gif",
+    "description": ""
   }
 }
 const projectShowcaseModal = document.getElementById('projectShowcaseModal');
@@ -14,7 +25,11 @@ projectShowcaseModal.addEventListener('show.bs.modal', function (event){
   let key = button.getAttribute('data-bs-key')
   let modalTitle = projectShowcaseModal.querySelector('.modal-title')
   let modalScreenshot = projectShowcaseModal.querySelector('.project-screenshot')
+  let modalLive = projectShowcaseModal.querySelector('.modal-live')
+  let modalRepo = projectShowcaseModal.querySelector('.modal-repo')
 
   modalTitle.textContent = showcaseModalData[key]['title']
   modalScreenshot.src = showcaseModalData[key]['path']
+  modalLive.href = showcaseModalData[key]['live']
+  modalRepo.href = showcaseModalData[key]['repo']
 })
